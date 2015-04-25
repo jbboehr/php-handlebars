b10ba78a2c115b19239f8dd48855bbdfe8e649a5
John Boehr <jbboehr@gmail.com>
2015-04-24 22:33:49 -0700

    Update stub

--- handlebars.stub.php
+++ handlebars.stub.php
@@ -1,68 +1,74 @@
 <?php 
 
-const HANDLEBARS_COMPILER_FLAG_NONE = 0;
-const HANDLEBARS_COMPILER_FLAG_USE_DEPTHS = (1 << 0);
-const HANDLEBARS_COMPILER_FLAG_STRING_PARAMS = (1 << 1);
-const HANDLEBARS_COMPILER_FLAG_TRACK_IDS = (1 << 2);
-const HANDLEBARS_COMPILER_FLAG_KNOWN_HELPERS_ONLY = (1 << 4);
-const HANDLEBARS_COMPILER_FLAG_COMPAT = (1 << 0);
-const HANDLEBARS_COMPILER_FLAG_ALL = (1 << 4) - 1;
+namespace Handlebars;
 
-/**
- * Get the last error that occurred.
- * 
- * @return string
- */
-function handlebars_error() {}
+const COMPILER_FLAG_NONE = 0;
+const COMPILER_FLAG_USE_DEPTHS = (1 << 0);
+const COMPILER_FLAG_STRING_PARAMS = (1 << 1);
+const COMPILER_FLAG_TRACK_IDS = (1 << 2);
+const COMPILER_FLAG_KNOWN_HELPERS_ONLY = (1 << 4);
+const COMPILER_FLAG_COMPAT = (1 << 0);
+const COMPILER_FLAG_ALL = (1 << 4) - 1;
 
-/**
- * Tokenize a template and return an array of tokens
- *
- * @param string $tmpl
- * @return array
- */
-function handlebars_lex($tmpl) {}
+class Native
+{
+    /**
+     * Get the last error that occurred.
+     * 
+     * @return string
+     */
+    public static function getLastError() {}
 
-/**
- * Tokenize a template and return a readable string representation of the tokens
- *
- * @param string $tmpl
- * @return string
- */
-function handlebars_lex_print($tmpl) {}
+    /**
+     * Tokenize a template and return an array of tokens
+     *
+     * @param string $tmpl
+     * @return array
+     */
+    public static function lex($tmpl) {}
 
-/**
- * Parse a template and return the AST
- *
- * @param string $tmpl
- * @return array
- */
-function handlebars_parse($tmpl) {}
+    /**
+     * Tokenize a template and return a readable string representation of the tokens
+     *
+     * @param string $tmpl
+     * @return string
+     */
+    public static function lexPrint($tmpl) {}
 
-/**
- * Parse a template and return a readable string representation of the AST
- * 
- * @param string $tmpl
- * @return string
- */
-function handlebars_parse($tmpl) {}
+    /**
+     * Parse a template and return the AST
+     *
+     * @param string $tmpl
+     * @return array
+     */
+    public static function parse($tmpl) {}
 
-/**
- * Compile a template and return the opcodes 
- * 
- * @param string $tmpl
- * @param integer $flags
- * @param array $knownHelpers
- * @return array
- */
-function handlebars_compile($tmpl, $flags = 0, array $knownHelpers = null) {}
+    /**
+     * Parse a template and return a readable string representation of the AST
+     * 
+     * @param string $tmpl
+     * @return string
+     */
+    public static function parsePrint($tmpl) {}
 
-/**
- * Compile a template and return a readable string representation of the opcodes 
- * 
- * @param string $tmpl
- * @param integer $flags
- * @param array $knownHelpers
- * @return string
- */
-function handlebars_compile_print($tmpl, $flags = 0, array $knownHelpers = null) {}
+    /**
+     * Compile a template and return the opcodes 
+     * 
+     * @param string $tmpl
+     * @param integer $flags
+     * @param array $knownHelpers
+     * @return array
+     */
+    public static function compile($tmpl, $flags = 0, array $knownHelpers = null) {}
+
+    /**
+     * Compile a template and return a readable string representation of the opcodes 
+     * 
+     * @param string $tmpl
+     * @param integer $flags
+     * @param array $knownHelpers
+     * @return string
+     */
+    public static function compilePrint($tmpl, $flags = 0, array $knownHelpers = null) {}
+}
+
