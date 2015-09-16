4c36882659b4e82b233984ad7e0768b83af2c06e
John Boehr <jbboehr@gmail.com>
2015-09-16 12:27:38 -0700

    Release v0.5.0

--- php_handlebars.h
+++ php_handlebars.h
@@ -11,10 +11,10 @@
 #endif
 
 #define PHP_HANDLEBARS_NAME "handlebars"
-#define PHP_HANDLEBARS_VERSION "0.4.3"
-#define PHP_HANDLEBARS_RELEASE "2015-06-23"
+#define PHP_HANDLEBARS_VERSION "0.5.0"
+#define PHP_HANDLEBARS_RELEASE "2015-09-16"
 #define PHP_HANDLEBARS_AUTHORS "John Boehr <jbboehr@gmail.com> (lead)"
-#define PHP_HANDLEBARS_SPEC "2.0.0"
+#define PHP_HANDLEBARS_SPEC "3.0.3"
 
 extern zend_module_entry handlebars_module_entry;
 #define phpext_handlebars_ptr &handlebars_module_entry
