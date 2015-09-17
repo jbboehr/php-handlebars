1b6de53709e247ac531c2aa36034cad079d3bfdf
John Boehr <jbboehr@gmail.com>
2015-09-17 09:25:10 -0700

    Fix travis build

---
+++ .travis.sh
@@ -0,0 +1,34 @@
+
+set -e
+
+if [ ! -f $HOME/build/include/check.h ]; then
+    wget http://downloads.sourceforge.net/project/check/check/0.9.14/check-0.9.14.tar.gz
+    tar xfv check-0.9.14.tar.gz
+    cd check-0.9.14
+    ./configure --prefix=$HOME/build
+    make
+    make install
+    cd ..
+    rm check-0.9.14.tar.gz
+fi
+
+if [ ! -f $HOME/build/bin/bison ]; then
+    wget http://gnu.mirror.iweb.com/bison/bison-3.0.2.tar.gz
+    tar xfv bison-3.0.2.tar.gz
+    cd bison-3.0.2
+    ./configure --prefix=$HOME/build
+    make
+    make install
+    cd ..
+    rm bison-3.0.2.tar.gz
+fi
+
+if [ ! -f $HOME/build/include/handlebars.h ]; then
+    git clone https://github.com/jbboehr/handlebars.c handlebars-c --recursive
+    cd handlebars-c
+    ./bootstrap
+    ./configure --prefix=$HOME/build
+    make install
+    cd ..
+fi
+
