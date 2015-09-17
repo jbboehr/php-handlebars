1b25eac614a512a9619f3882a1d91035e5a18413
John Boehr <jbboehr@gmail.com>
2015-09-17 17:03:32 +0000

    Release v0.5.1

--- php_handlebars.h
+++ php_handlebars.h
@@ -11,8 +11,8 @@
 #endif
 
 #define PHP_HANDLEBARS_NAME "handlebars"
-#define PHP_HANDLEBARS_VERSION "0.5.0"
-#define PHP_HANDLEBARS_RELEASE "2015-09-16"
+#define PHP_HANDLEBARS_VERSION "0.5.1"
+#define PHP_HANDLEBARS_RELEASE "2015-09-17"
 #define PHP_HANDLEBARS_AUTHORS "John Boehr <jbboehr@gmail.com> (lead)"
 #define PHP_HANDLEBARS_SPEC "3.0.3"
 
