4a1c92d8b858b9212f1c0b255fbd48f0d4e4809c
John Boehr <jbboehr@gmail.com>
2015-10-31 08:15:18 +0000

    Upgrade to handlebars v4.0.4

--- php_handlebars.h
+++ php_handlebars.h
@@ -8,7 +8,7 @@
 #define PHP_HANDLEBARS_VERSION "0.6.0"
 #define PHP_HANDLEBARS_RELEASE "2015-10-28"
 #define PHP_HANDLEBARS_AUTHORS "John Boehr <jbboehr@gmail.com> (lead)"
-#define PHP_HANDLEBARS_SPEC "4.0.2"
+#define PHP_HANDLEBARS_SPEC "4.0.4"
 
 extern zend_module_entry handlebars_module_entry;
 #define phpext_handlebars_ptr &handlebars_module_entry
