a48fd8f5de6fb9b6c8a2b3c54fba102c749bd6c7
John Boehr <jbboehr@gmail.com>
2016-04-04 13:36:40 -0700

    Add more cache stats

--- php_handlebars.c
+++ php_handlebars.c
@@ -145,18 +145,46 @@ static PHP_MINFO_FUNCTION(handlebars)
     php_info_print_table_row(2, "libhandlebars Mustache Spec Version", handlebars_mustache_spec_version_string());
     php_info_print_table_row(2, "PSR support", handlebars_has_psr ? "active" : "inactive");
     snprintf(buf, sizeof(buf), "%ld", talloc_total_size(HANDLEBARS_G(root)));
-    php_info_print_table_row(2, "Memory usage", buf);
-    //if( HANDLEBARS_G(cache) ) {
-        snprintf(buf, sizeof(buf), "%ld", stat.current_entries);
-        php_info_print_table_row(2, "Cache entries", buf);
-        snprintf(buf, sizeof(buf), "%ld", stat.current_size);
-        php_info_print_table_row(2, "Cache size", buf);
-        snprintf(buf, sizeof(buf), "%ld", stat.hits);
-        php_info_print_table_row(2, "Cache hits", buf);
-        snprintf(buf, sizeof(buf), "%ld", stat.misses);
-        php_info_print_table_row(2, "Cache misses", buf);
-    //}
+    php_info_print_table_row(2, "Local memory usage", buf);
     php_info_print_table_end();
+
+    //if( HANDLEBARS_G(cache) ) {
+    php_info_print_table_start();
+    php_info_print_table_colspan_header(2, "Cache");
+    php_info_print_table_row(2, "Backend", stat.name);
+
+    snprintf(buf, sizeof(buf), "%ld", stat.total_size);
+    php_info_print_table_row(2, "Block size", buf);
+
+    snprintf(buf, sizeof(buf), "%ld", stat.total_table_size);
+    php_info_print_table_row(2, "Table size", buf);
+
+    snprintf(buf, sizeof(buf), "%ld", stat.total_entries);
+    php_info_print_table_row(2, "Table entries", buf);
+
+    snprintf(buf, sizeof(buf), "%ld", stat.current_entries);
+    php_info_print_table_row(2, "Table entries used", buf);
+
+    snprintf(buf, sizeof(buf), "%ld", stat.total_entries > 0 ? stat.total_entries - stat.current_entries : 0);
+    php_info_print_table_row(2, "Table entries free", buf);
+
+    snprintf(buf, sizeof(buf), "%ld", stat.total_data_size);
+    php_info_print_table_row(2, "Data segment size", buf);
+
+    snprintf(buf, sizeof(buf), "%ld", stat.current_data_size);
+    php_info_print_table_row(2, "Data segment used", buf);
+
+    snprintf(buf, sizeof(buf), "%ld", stat.total_data_size > 0 ? stat.total_data_size - stat.current_table_size : 0);
+    php_info_print_table_row(2, "Data segment free", buf);
+
+    snprintf(buf, sizeof(buf), "%ld", stat.hits);
+    php_info_print_table_row(2, "Hits", buf);
+
+    snprintf(buf, sizeof(buf), "%ld", stat.misses);
+    php_info_print_table_row(2, "Misses", buf);
+
+    php_info_print_table_end();
+    //}
 
     DISPLAY_INI_ENTRIES();
 }
