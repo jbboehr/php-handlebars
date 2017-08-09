041f2f788460742f81b494d2afed91f0face5e38
John Boehr <jbboehr@gmail.com>
2017-08-08 19:01:32 -0700

    Experimenting with build cache

---
+++ .appveyor.bat
@@ -0,0 +1,25 @@
+
+if "%1" == "install" (
+	REM dirent.h
+	set DIRENT_REPO_DIR="%BUILD_CACHE_DIR%\dirent"
+	if not exist "%DIRENT_REPO_DIR%" (
+		git clone https://github.com/jbboehr/dirent.git %DIRENT_REPO_DIR%
+	) else (
+		cd %DIRENT_REPO_DIR%
+		git fetch origin
+		git checkout --force origin/master
+	)
+	copy /Y %DIRENT_REPO_DIR%\include\dirent.h %ARTIFACT_DIR%\include
+
+	REM getopt.h
+	set GETOPT_REPO_DIR="%BUILD_CACHE_DIR%\getopt"
+	if not exist "%GETOPT_REPO_DIR%" (
+		git clone https://github.com/jbboehr/Getopt-for-Visual-Studio.git %GETOPT_REPO_DIR%
+	) else (
+		cd %DIRENT_REPO_DIR%
+		git fetch origin
+		git checkout --force origin/master
+	)
+	copy /Y %GETOPT_REPO_DIR%\getopt.h %ARTIFACT_DIR%\include
+)
+
