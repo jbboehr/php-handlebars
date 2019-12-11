6ab6f7b60ecd74a3c0976cc6ea657c69197bede9
John Boehr <john@johnboehr.is>
2019-12-10 22:51:39 -0800

    Fix test generation for Hercules CI

--- ci.nix
+++ ci.nix
@@ -1,12 +1,26 @@
 let
     generateHandlebarsDrv = { pkgs, php }:
+        let
+            mustache_spec = pkgs.callPackage (import ((fetchTarball {
+                url = https://github.com/jbboehr/mustache-spec/archive/5b85c1b58309e241a6f7c09fa57bd1c7b16fa9be.tar.gz;
+                sha256 = "1h9zsnj4h8qdnzji5l9f9zmdy1nyxnf8by9869plyn7qlk71gdyv";
+            }))) {};
+
+            handlebars_spec = pkgs.callPackage (import ((fetchTarball {
+                url = https://github.com/jbboehr/handlebars-spec/archive/9306f3062b4b03d5179c33834bee7263a621639a.tar.gz;
+                sha256 = "0gn9v88fxdf0wwgd8ix1xfxvif89mgfhxkp316arv5ljbibx2x9h";
+            }))) {};
+        in
         pkgs.runCommand "pecl-handlebars.tgz" {
             buildInputs = [ php ];
             src = builtins.filterSource
                 (path: type: baseNameOf path != ".idea" && baseNameOf path != ".git" && baseNameOf path != "ci.nix")
                 ./.;
+            HANDLEBARS_SPEC_DIR = "${handlebars_spec}/share/handlebars-spec";
+            MUSTACHE_SPEC_DIR = "${mustache_spec}/share/mustache-spec";
         } ''
             cp -r $src/* .
+            chmod -R +w tests
             php generate-tests.php
             pecl package | tee tmp.txt
             pecl_tgz=$(cat tmp.txt | grep -v Warning | awk '{print $2}')
@@ -35,6 +49,8 @@ builtins.mapAttrs (k: _v:
     };
   in
   pkgs.recurseIntoAttrs {
+    peclDist = phpHandlebarsSrc;
+
     php71 = let
         path = builtins.fetchTarball {
            url = https://github.com/NixOS/nixpkgs/archive/release-19.03.tar.gz;
